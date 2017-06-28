c8f74c36fb094d319d3c09ff59850ed290fce68e
adriengodoy <mr.a.godoy@gmail.com>
2017-06-28 21:36:29 +0200

    ajout de fonctions permettant de recupere dans un objet les entites analysees par  recast.  ajout de fonctions facilitant la creation de messages enrichis (il en  reste a faire)

--- src/message.js
+++ src/message.js
@@ -52,31 +52,23 @@ const replyMessage = (message) =>
     .then(function(res) 
     {
       var intent = res.intent()
-      //var parsedJSON = JSON.parse(res)
-      console.log("intent : "+JSON.stringify(intent))
-      console.log("message JSON: "+JSON.stringify(message))
-      console.log("res : "+JSON.stringify(res))
-      //console.log("ENTITY GREETING : "+JSON.stringify(greeting))
-      console.log("ENTITY GREETING : "+ res.entities.greeting[0].value)
+      //console.log("intent : "+JSON.stringify(intent)) // l'intention liée au message
+      //console.log("message JSON: "+JSON.stringify(message)) // le corps du message
+      //console.log("res : "+JSON.stringify(res)) // reçoit le JSON entier
+      //console.log("ENTITY GREETING : "+ res.entities.greeting[0].value) // récupère la value de la première entité greeting
+      console.log(getKeys(res));
+      var entitiesArray = getEntities(res);
+      var buttons = new Array();
+      var btn0 = new Array();
+      btn0['title'] = 'btn0 title';
+      btn0['value'] = 'btn0 value';
+      buttons[0] = btn0;
+      var btn1 = new Array();
+      btn1['title'] = 'btn1 title';
+      btn1['value'] = 'btn1 value';
+      buttons[1] = btn1;
+      makeQuickReply('quickRep TEST', buttons)
     });
-    // récupère la liste des entités
-    /*var requestEntities = require('superagent');
-    var urlEnt = "https:";
-    urlEnt += "/";
-    urlEnt += "/";
-    urlEnt += "api.recast.ai/v2/entities";
-    requestEntities.get(urlEnt)
-    .send()
-    .set('Authorization', 'Token fca3215abc3eeec7df0ca5fcd4e943d6')
-    .end((err, res) => 
-      {
-        console.log("Entities : "+res.text);
-        var fs = require('fs'); // pour écrire dans un fichier
-        fs.writeFile("entitiesTEST.txt", res.text, function (err) 
-        {
-          if (err) throw err;
-        });
-      });*/
     //////////////////  \TEST  /////////////////////////
 
 
@@ -113,3 +105,236 @@ const replyMessage = (message) =>
 }
 
 module.exports = replyMessage
+
+
+
+///////////////// Functions //////////////////////
+
+/*
+ * Cette fonction renvoie un tableau associatif contenant les clés et
+ * valeurs du json passé en paramètres
+ */
+function getKeys(jsonData) 
+{
+  var key;
+  var val;
+  var convertedJSON = new Array();
+  for (var i in jsonData)
+  {
+    key = i;
+    val = jsonData[i];
+    convertedJSON[key] = val;
+  }
+  return convertedJSON;
+};
+
+/*
+ * Cette fonction extrait la liste des entités ainsi que leur valeur
+ * et les retourne dans un tableau associatif
+ * par exemple, si le message contient "bonjour" et que dans Recast.AI
+ * nous avons une entité qui s'appelle "greeting" qui contient le mot "bonjour"
+ * alors myArray["greeting"]["value"] == "bonjour"
+ */
+function getEntities(jsonData)
+{
+  var arr = [];
+  var subArr = new Array();
+  var jsonArray = getKeys(jsonData);
+  var ent = jsonArray["entities"];
+  var key;
+  var subKey;
+  var subsubKey;
+  var val;
+  var subVal;
+  var subsubVal;
+  if (ent != null)
+  {
+    //console.log("ent n'est pas nul");
+    for(var x in ent)
+    {
+      key = x;
+      //console.log("key = "+key);
+      val = ent[key];
+      //console.log("val = "+JSON.stringify(val));
+      //console.log("val.length = "+val.length);
+      for(var y in val)
+      {
+        subKey = y;
+        //console.log("subKey = "+subKey);
+        subVal = val[subKey];
+        //console.log("subVal = "+JSON.stringify(subVal));
+        for (var z in subVal)
+        {
+          subsubKey = z;
+          //console.log("subsubKey = "+subsubKey);
+          subsubVal = subVal[subsubKey];
+          //console.log("subsubVal = "+subsubVal);
+          subArr[subsubKey] = subsubVal;
+          arr[key] = subArr;
+          //console.log('arr["'+key+'"]["'+subsubKey+'"] = '+subsubVal);
+          //console.log("vérification : "+arr[key][subsubKey]);
+        }
+      }
+      //console.log('arr["greeting"]["value"] = '+arr["greeting"]["value"]);
+      //console.log("arr.length = "+Object.keys(arr).length);
+      //console.log("version string de arr = "+JSON.stringify(arr));
+    } 
+  }
+  //console.log("ent = "+JSON.stringify(ent));
+  //console.log("ent.length = "+ent.length);
+  return arr;
+};
+
+/*
+ * Cette fonction aide à créer des boutons
+ * retourne un json, que l'on peut ensuite ajouter à un tableau 
+ * pour l'intégrer dans un message enrichi
+ */
+function makeBtn(title, value)
+{
+  var btn = {};
+  btn['title'] = title;
+  btn['value'] = value;
+  return btn;
+};
+
+/*
+ * Cette fonction permet de créer des messages de type "quick replies"
+ * 
+ * paramètres:
+ * - title : un string
+ * - buttons: un array de la forme buttons[0][title], buttons[0][value]
+ *   la première dimension est l'indice du bouton. Chaque bouton doit avoir
+ *   un titre et une valeur
+ */
+function makeQuickReply(btntitle, buttons)
+{
+  var quickRep = {};
+  quickRep['type'] = 'quickReplies';
+  quickRep['content'] = {title : btntitle};
+  quickRep['content']['buttons'] = [];
+  if(Object.keys(buttons).length > 0 )
+  {
+    for (var i=0; i<Object.keys(buttons).length; i++)
+    {
+      var btn = {};
+      btn.title = buttons[i]['title'];
+      btn.value = buttons[i]['value'];
+      quickRep['content']['buttons'].push(btn);
+    }
+  }
+  console.log('makeQuickReply() result : '+JSON.stringify(quickRep));
+  return quickRep;
+};
+
+/*
+ * cette fonction sert à faire les messages de type "card"
+ *
+ * paramètres:
+ * - cardTitle: le titre (un string)
+ * - cardSubtitle: le sous-titre (un string)
+ * - imageUrl: l'adresse de l'image (un string)
+ * - buttons: un tableau qui contient des boutons (faits avec la fonction makeBtnMsg)
+ * 
+ */
+function makeCard(cardTitle, cardSubtitle, cardImageUrl, buttons)
+{
+  var card = {};
+  card['type'] = 'card';
+  card['content'] = {title: cardTitle, subtitle: cardSubtitle, imageUrl: cardImageUrl};
+  card['content']['buttons'] = [];
+  if(Object.keys(buttons).length > 0)
+  {
+    for (var i=0; i<Object.keys(buttons).length; i++)
+    {
+      var btn = {};
+      btn.title = buttons[i]['title'];
+      btn.value = buttons[i]['value'];
+      card['content']['buttons'].push(btn);
+    }
+  }
+  return card;
+};
+
+/*
+ * cette fonction sert à faire les messages de type "button"
+ *
+ * paramètres:
+ * - btnMsgTitle: le titre (un string)
+ * - buttons: le tableau qui contient les boutons
+ * 
+ */
+function makeBtnMsg(btnMsgTitle, buttons)
+{
+  var btnMsg = {};
+  btnMsg['type'] = 'card';
+  btnMsg['content'] = {title: btnMsgTitle};
+  btnMsg['content']['buttons'] = [];
+  if(Object.keys(buttons).length > 0)
+  {
+    for(var i=0; i<Object.keys(buttons).length; i++)
+    {
+      var btn = {};
+      btn.title = buttons[i]['title'];
+      btn.value = buttons[i]['value'];
+      card['content']['buttons'].push(btn);
+    }
+  }
+  return btnMsg;
+};
+
+/*
+ * Cette fonction sert à faire des card pour un carousel.
+ * ces cartes doivent être assignées à un tableau
+ *
+ * paramètres:
+ * - cardTitle: un string, le titre de la card
+ * - cardImageUrl: l'url de l'image de la card
+ * - buttons: un tableau qui contient des boutons (faits avec la fonction makeBtnMsg)
+ */
+function makeCardForCarousel(cardTitle, cardImageUrl, buttons)
+{
+  var card = {};
+  card = {title: title, imageUrl: cardImageUrl};
+  card['buttons'] = [];
+  if(Object.keys(buttons).length > 0)
+  {
+    for (var i=0; i<Object.keys(buttons).length; i++)
+    {
+      var btn = {};
+      btn.title = buttons[i]['title'];
+      btn.value = buttons[i]['value'];
+      card['content']['buttons'].push(btn);
+    }
+  }
+  return card;
+};
+
+/*
+ * Cette fonction sert à faire des messages de type "carousel"
+ *
+ * paramètres:
+ * - carouselTitle: le titre du carousel
+ * - cardArray: un tableau de card (faits avec la fonction makeCardForCarousel)
+ */
+function makeCarousel(carouselTitle, cardArray)
+{
+  var carousel = {};
+  carousel['type'] = 'carousel';
+  carousel['content'] = cardArray;
+  return carousel;
+}
+
+/*
+ * Cette fonction sert à créer les éléments qui seront ajoutés à un tableau d'éléments
+ *
+ * paramètres:
+ * - elTitle: le titre de l'élément
+ * - imageUrl: l'url de l'image
+ * - subtitle: le sous-titre (quoi, vous codez et vous ne parlez pas anglais? seriously???)
+ * - buttons:  un tableau qui contient des boutons (faits avec la fonction makeBtnMsg)
+ */
+function makeElement(elTitle, imageUrl, subtitle, buttons)
+{
+  
+}
